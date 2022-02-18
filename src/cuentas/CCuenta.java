ac9b0ce46db03a6b4d4332a7566b46d2d85d96e9
Silvia <Silvia@DESKTOP-D06UC3E.home>
2022-02-18 21:55:34 +0100

    Se han agregado los comentarios de javaDoc.

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -5,19 +5,44 @@
 package cuentas;
 
 /**
- *
- * @author Silvia
+ * Clase cuenta que guarda en su interior el nombre, la cuenta, el saldo y el tipo de interes.
+ * @author Silvia Olivares Amer 
+ * @version 1.0
+ * @since 1.0
+ * @see <a target="_blank" href="https://github.com/Silviafp/Deposito/tree/master/src/cuentas">Github</a>
  */
 public class CCuenta {
+    /**
+     * Nombre del cliente de la cuenta
+     */
     private String nombre;
+    /**
+     * Identificador de la cuenta
+     */
     private String cuenta;
+    /**
+     * Saldo de la cuenta
+     */
     private double saldo;
+    /**
+     * Tipo de interes de la cuenta
+     */
     private double tipoInteres;
 
+    /**
+     * Este es el constructor sin parametros.
+     */
     public CCuenta()
     {
     }
-
+    
+    /**
+     * Este es el constructor con parametros.
+     * @param nombre este es el nombre del usuario de la cuenta.
+     * @param cuenta este es el numero de cuenta.
+     * @param saldo el saldo inicial.
+     * @param tipoInteres tipo de interes inicial.
+     */
     public CCuenta(String nombre, String cuenta, double saldo, double tipoInteres)
     {
         this.nombre = nombre;
@@ -26,6 +51,12 @@ public class CCuenta {
         this.tipoInteres = tipoInteres;
     }
 
+    /**
+     * Metodo ingresar: envian una cantidad de tipo double y la guarda en el objeto.
+     * 
+     * @param cantidad Cantidad que va a sumar al saldo actual.
+     * @throws java.lang.Exception Si la cantidad es menor a 0, devuelve una excepcion.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -33,6 +64,13 @@ public class CCuenta {
         saldo = saldo + cantidad;
     }
 
+    /**
+     * Metodo retirar: envian una cantidad de tipo double y lo resta del objeto.
+     * 
+     * @param cantidad Cantidad que va a retirar del saldo actual.
+     * @throws java.lang.Exception Si la cantidad es menor o igual a 0 o saldo es menor a cantidad, devuelve su correspondiente excepcion.
+     * 
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -42,34 +80,67 @@ public class CCuenta {
         saldo = saldo - cantidad;
     }
 
+    /**
+     * Getter del nombre.
+     * @return nombre.
+     */
     public String getNombre() {
         return nombre;
     }
-
+    
+    /**
+     * Setter del nombre.
+     * @param nombre Cambio de nombre del usuario.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Getter de cuenta.
+     * @return cuenta.
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     * Setter de la cuenta.
+     * @param cuenta Identificador de la cuenta.
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
-
+    
+    /**
+     * Getter del saldo.
+     *
+     * @return saldo.
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * Setter del saldo.
+     * @param saldo Saldo actual de la cuenta.
+     */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
+    /**
+     * Getter del tipoInteres.
+     * @return tipoInteres
+     */
     public double getTipoInteres() {
         return tipoInteres;
     }
-
+    
+    /**
+     * Setter del tipoInteres.
+     * @param tipoInteres Tipo de interes de la cuenta.
+     */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
